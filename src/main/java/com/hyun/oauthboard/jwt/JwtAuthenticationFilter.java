6b4d144b19b0ea794746ff08cc17a80d32c36d83
leeuihyun <meta212121@naver.com>
2025-05-19 17:53:44 +0900

    refactor : 로그인이 필요하지 않은 페이지에서는 JWT 토큰 검사하지 않게 수정 /home, /login과 같은 페이지는 토큰 검사 X

--- src/main/java/com/hyun/oauthboard/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hyun/oauthboard/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.hyun.oauthboard.jwt;
 
+import com.hyun.oauthboard.security.SecurityConstants;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -19,24 +20,31 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
         throws IOException, ServletException {
-        // 1. Request Header에서 JWT 토큰 추출
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String requestURI = httpServletRequest.getRequestURI();
+
+        if (SecurityConstants.EXCLUDE_URLS.stream()
+            .anyMatch(requestURI::startsWith)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         String token = resolveToken((HttpServletRequest) request);
-        
+
         System.out.println("토큰 = " + token);
-        // 2. validateToken으로 토큰 유효성 검사
+
         if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext에 저장
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         chain.doFilter(request, response);
     }
 
-    // Request Header에서 토큰 정보 추출
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
--- src/main/java/com/hyun/oauthboard/jwt/JwtTokenProvider.java
+++ src/main/java/com/hyun/oauthboard/jwt/JwtTokenProvider.java
@@ -40,7 +40,7 @@ public class JwtTokenProvider {
         long now = (new Date()).getTime();
 
         // Access Token
-        Date accessTokenExpiresIn = new Date(now + 86400000);
+        Date accessTokenExpiresIn = new Date(now + 3600000);
         String accessToken = Jwts.builder()
             .setSubject(authentication.getName())
             .claim("auth", authorities)

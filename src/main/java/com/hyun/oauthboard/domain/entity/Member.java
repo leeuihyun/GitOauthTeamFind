7766c3be63fa11546c4f071143d35335e2d6caed
leeuihyun <meta212121@naver.com>
2025-06-10 20:27:29 +0900

    refactor : 부모 연관관계 @Builder.Default 설정 테스트 코드 작성 용이하기 위함

--- src/main/java/com/hyun/oauthboard/domain/entity/Member.java
+++ src/main/java/com/hyun/oauthboard/domain/entity/Member.java
@@ -2,23 +2,29 @@ package com.hyun.oauthboard.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hyun.oauthboard.domain.dto.github.GithubMemberInfo;
-import com.hyun.oauthboard.domain.entity.model.BaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
+@Builder
 @ToString(exclude = "boards")
 @Getter
-@NoArgsConstructor
-public class Member extends BaseTimeEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
 
     @Id
     @Column(name = "member_id")
@@ -31,8 +37,13 @@ public class Member extends BaseTimeEntity {
     private String memberAvatar;
 
     @JsonIgnore
+    @Builder.Default
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Board> boards = new ArrayList<>();
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 
     public Member(GithubMemberInfo githubMemberInfo) {
         this.memberId = githubMemberInfo.getId();

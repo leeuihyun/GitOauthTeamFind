5c84c0a979e7b59d2fc42633632a97bb019f8fdc
leeuihyun <meta212121@naver.com>
2025-06-20 20:57:27 +0900

    refactor : N+1 문제 해결 queryDsl을 도입하여 가독성 / 직관성 좋은 코드로 N+1 문제 해결

--- src/main/java/com/hyun/oauthboard/domain/board/repository/BoardCustomRepositoryImpl.java
+++ src/main/java/com/hyun/oauthboard/domain/board/repository/BoardCustomRepositoryImpl.java
@@ -1,16 +1,13 @@
 package com.hyun.oauthboard.domain.board.repository;
 
 import static com.hyun.oauthboard.domain.board.entity.QBoard.board;
-import static com.hyun.oauthboard.domain.board.entity.QBoardLike.boardLike;
-import static com.hyun.oauthboard.domain.board.entity.QBoardView.boardView;
+import static com.hyun.oauthboard.domain.boardLike.entity.QBoardLike.boardLike;
+import static com.hyun.oauthboard.domain.boardView.entity.QBoardView.boardView;
 import static com.hyun.oauthboard.domain.member.entity.QMember.member;
 
 import com.hyun.oauthboard.domain.board.dto.BoardResponse;
-import com.hyun.oauthboard.domain.board.entity.Board;
-import com.hyun.oauthboard.enums.BoardError;
-import com.hyun.oauthboard.exception.CustomException;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -22,33 +19,33 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository {
 
     @Override
     public BoardResponse findBoardLeftJoinViewsAndLikes(Long boardId) {
-        Board findBoard = Optional.ofNullable(jpaQueryFactory
-            .selectFrom(board)
-            .join(board.member, member).fetchJoin()
+
+        return jpaQueryFactory
+            .select(
+                Projections.constructor(BoardResponse.class,
+                    board.boardId,
+                    member.memberId,
+                    board.boardTitle,
+                    board.boardContent,
+                    boardView.viewId.count().coalesce(0L),
+                    boardLike.likeId.count().coalesce(0L),
+                    board.createdAt,
+                    board.updatedAt
+                )
+            )
+            .from(board)
+            .join(board.member, member)
+            .leftJoin(boardLike).on(boardLike.board.boardId.eq(board.boardId))
+            .leftJoin(boardView).on(boardView.board.boardId.eq(board.boardId))
             .where(board.boardId.eq(boardId))
-            .fetchOne()).orElseThrow(() -> new CustomException(BoardError.BOARD_ID_NOT_EXIST));
-
-        Long likes = Optional.ofNullable(jpaQueryFactory
-            .select(boardLike.likeId.count())
-            .from(boardLike)
-            .where(boardLike.board.boardId.eq(boardId))
-            .fetchOne()).orElse(0L);
-
-        Long views = Optional.ofNullable(jpaQueryFactory
-            .select(boardView.viewId.count())
-            .from(boardView)
-            .where(boardView.board.boardId.eq(boardId))
-            .fetchOne()).orElse(0L);
-
-        return BoardResponse.builder()
-            .boardId(findBoard.getBoardId())
-            .memberId(findBoard.getMember().getMemberId())
-            .boardTitle(findBoard.getBoardTitle())
-            .boardContent(findBoard.getBoardContent())
-            .likes(likes)
-            .views(views)
-            .createdAt(findBoard.getCreatedAt())
-            .updatedAt(findBoard.getUpdatedAt())
-            .build();
+            .groupBy(
+                board.boardId,
+                member.memberId,
+                board.boardTitle,
+                board.boardContent,
+                board.createdAt,
+                board.updatedAt
+            )
+            .fetchOne();
     }
 }
